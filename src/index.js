369b9a1bc4d0f93eb673f22098307cb7ad2870e8
thesloppyguy <sahilmandal@outlook.com>
2023-10-09 03:29:30 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,19 +3,8 @@ import * as model from "./model.js";
 
 const main = async () => {
   try {
-    core.info("Getting current issue...");
-    const issue = await model.getCurrentIssue();
-    core.debug(issue);
-    // if (issue.state === "close") {
-    //   const isBlocked = await model.update(issue);
-
-    //   if (isBlocked === undefined) core.info("No blocking issues found.");
-    //   else if (isBlocked === true) core.setFailed("Issue/PR is blocked.");
-    //   else if (isBlocked === false) core.info("Issue/PR is not blocked.");
-    // } else {
-    //   core.info("Issue is closed. Unblocking other issues...");
-    //   await model.unblockPRs(issue.number);
-    // }
+    core.info("Handling Dependencies...");
+    await model.update();
   } catch (error) {
     core.setFailed(error.message);
   }
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,6 @@
-const regex = /blocked by:? ([#\d, ]+)/gi;
+const regex = /Dependent on:? ([#\d, ]+)/gi;
 
-export const signature =
-  "This comment was automatically written by the [Blocking Issues](https://github.com/Levi-Lesches/blocking-issues) bot, and this PR will be monitored for further progress.";
-export const defaultLabel = {
-  name: "blocked",
-  color: "000000",
-  description: "Waiting for another PR/issue to be merged/closed.",
-};
-
-export function parseBlockingIssues(body) {
+export function findDependencies(body) {
   const issues = [];
   if (body === null) return issues;
   for (const match of body.matchAll(regex)) {
@@ -18,23 +10,4 @@ export function parseBlockingIssues(body) {
     }
   }
   return issues;
-}
-
-export function getCommentText(blockingIssues, openIssues, brokenIssues) {
-  let status = "Ready to merge :heavy_check_mark:";
-  if (brokenIssues.length > 0) status = "Error :warning:";
-  else if (openIssues.length > 0) status = "Blocked :x:";
-  var result = "";
-  result += `# Status: ${status}\n`;
-  result += "### Issues blocking this PR: \n";
-  for (const issue of blockingIssues) {
-    let symbol = ":heavy_check_mark:";
-    if (openIssues.includes(issue)) symbol = ":x:";
-    else if (brokenIssues.includes(issue))
-      symbol = ":warning: Issue/PR not found";
-    result += `- #${issue} ${symbol}\n`;
-  }
-  result += "----\n";
-  result += signature;
-  return result;
 }
